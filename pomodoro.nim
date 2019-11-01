7195fb7635713a1c56061b286bcc0c9ca5164f41
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2019-11-02 00:54:28 +0200

    making  things simpler

--- pomodoro.nim
+++ pomodoro.nim
@@ -1,48 +1,26 @@
 import terminal
 import os
 
-const pomodoroTime = 25
-const shortBreak = 5
-const longBreak = 10
-const seconds = 59
-const shortBreakState = 0
-const pomodoroState = 1
+const minutes = [25, 5]
+const texts = ["pomodoro", "short break"]
 
-var currentState = pomodoroState
-var text = ""
+var arrayIndex = 0
 
-proc countDownSeconds(minutes: int): void =
-  var secondsPast = 0
-
-  while true:
-    if minutes < 10:
-      text = "0" & $minutes & ":" 
-    else:
-      text = $minutes & ":" 
-
-    if (seconds - secondsPast) < 10:
-      text.add("0" & $(seconds - secondsPast))
-    else:
-      text.add($(seconds - secondsPast))
-
-    setCursorXPos(0)
-    stdout.write(text)
-    sleep(1000)
-    secondsPast += 1
-    if (seconds - secondsPast) <= 0: break
-
-proc countDownMinutes(minutes: int): void =
-  for i in 0..(minutes - 1):
-    countDownSeconds(minutes - i)
+proc twoDigit(number: int): string =
+  if number < 10:
+    result = "0" & $number
+  else:
+    result = $number
 
 while true:
-  if currentState == pomodoroState:
-    echo "press any key to start pomodoro"
-    discard getch()
-    countDownMinutes(pomodoroTime)
-    currentState = shortBreakState
-  else:
-    echo "press any key to start short break"
-    discard getch()
-    countDownMinutes(shortBreak)
-    currentState = pomodoroState
+  echo "press any key to start " & texts[arrayIndex]
+  discard getch()
+
+  for minutesLeft in countdown(minutes[arrayIndex] - 1, 0):
+    for secondsLeft in countdown(59, 0):
+      setCursorXPos(0)
+      stdout.write(twoDigit(minutesLeft) & ":" & twoDigit(secondsLeft))
+      sleep(1000)
+
+  arrayIndex = (arrayIndex + 1) mod 2
+  echo ""
