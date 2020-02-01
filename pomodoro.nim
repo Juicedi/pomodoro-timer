aeec3d5d04b280d39cd0e11343c8d6954d6c6498
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2020-02-01 19:47:52 +0200

    testing tuples and removed setCursorYPos

--- pomodoro.nim
+++ pomodoro.nim
@@ -2,10 +2,13 @@ import terminal
 import os
 import progressbar
 
-const minutes = [25, 5]
-const texts = ["pomodoro", "short break"]
+const pomodoro = [
+  (minutes: 25, text: "pomodoro"),
+  (minutes: 5, text: "short break")
+]
 
-var arrayIndex = 0
+var running = true
+var arrayIndex: int
 var maxSeconds: int
 var currentSeconds: int
 var termWidth = terminalWidth()
@@ -16,17 +19,17 @@ proc twoDigit(number: int): string =
   else:
     result = $number
 
-while true:
-  echo "press any key to start " & texts[arrayIndex]
+while running == true:
+  echo "press any key to start " & pomodoro[arrayIndex].text
   discard getch()
   eraseScreen()
-  setCursorYPos(0)
+  setCursorPos(0, 0)
 
-  for minutesLeft in countdown(minutes[arrayIndex] - 1, 0):
+  for minutesLeft in countdown(pomodoro[arrayIndex].minutes - 1, 0):
     for secondsLeft in countdown(59, 0):
       setCursorXPos(0)
       stdout.writeLine(twoDigit(minutesLeft) & ":" & twoDigit(secondsLeft))
-      maxSeconds = minutes[arrayIndex] * 60
+      maxSeconds = pomodoro[arrayIndex].minutes * 60
       currentSeconds = (minutesLeft * 60) + secondsLeft
       printProgress(maxSeconds - currentSeconds, maxSeconds)
       cursorUp(stdout)
@@ -34,7 +37,7 @@ while true:
       if termWidth != terminalWidth():
         termWidth = terminalWidth()
         eraseScreen()
-        setCursorYPos(0)
+        setCursorPos(0, 0)
 
   arrayIndex = (arrayIndex + 1) mod 2
   echo ""
